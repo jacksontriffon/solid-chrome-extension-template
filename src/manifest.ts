08b3e84d9f8433f09a57ec3437a8472e19b1f6ee
jacksontriffon <jackson.triffon@gmail.com>
2024-09-28 21:53:57 +0900

    fix: remove new tab

--- src/manifest.ts
+++ src/manifest.ts
@@ -19,9 +19,6 @@ const manifest = defineManifest(async () => ({
     default_popup: "src/pages/popup/index.html",
     default_icon: "icons/34x34.png",
   },
-  chrome_url_overrides: {
-    newtab: "src/pages/newtab/index.html",
-  },
   icons: {
     "128": "icons/128x128.png",
   },
